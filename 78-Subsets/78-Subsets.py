847d0d07e99e925a7d710383781c640d9b1f561c
Emily Yu <121976605+00eemsy@users.noreply.github.com>
2026-01-04 15:07:11 -0800

    File updated at 1/4/2026, 3:07:10 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,22 +1,16 @@
-# Last updated: 1/4/2026, 2:50:25 PM
+# Last updated: 1/4/2026, 3:07:10 PM
 1class Solution:
 2    def subsets(self, nums: List[int]) -> List[List[int]]:
-3        return self.powerSet(nums, [])
-4        
-5    def powerSet(self, arr, s):
-6        if arr not in s:
-7            s.append(arr)
-8
-9        for i in range(len(arr)):
-10            new_arr = []
-11
-12            if i == 0:
-13                new_arr = arr[i+1:]
-14            elif i == len(arr) - 1:
-15                new_arr = arr[:i]
-16            else:
-17                new_arr = arr[:i] + arr[i+1:]
-18
-19            self.powerSet(new_arr, s)
-20
-21        return s
+3        res = []
+4
+5        def helper(i, curr):
+6            if i == len(nums):
+7                res.append(curr[::])
+8                return
+9            curr.append(nums[i])
+10            helper(i+1, curr)
+11            curr.pop()
+12            helper(i+1, curr)
+13
+14        helper(0, [])
+15        return res
